7c580ceb5dc43942f03f1f63fb3893e60b4924ad
joysdev <joyswork@protonmail.com>
2024-07-16 19:07:42 -0600

    Verbal expression of how I expect to solve the problem

---
+++ main.js
@@ -0,0 +1,2 @@
+/*For Step 2, I will need a function that returns the corresponding letter grade, so it will need to check if the variable is both greater and less than the lower or higher grade respectively, except for the highest grade (only greater than) and the lowest grade (only less than) */
+
