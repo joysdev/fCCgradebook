724ce6a7ff55759dfc84567718cbefa126277b94
joysdev <joyswork@protonmail.com>
2024-07-16 19:17:41 -0600

    add function from problem, edit verbal because the highest grade is not a range, write all the conditions

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-/*For Step 2, I will need a function that returns the corresponding letter grade, so it will need to check if the variable is both greater and less than the lower or higher grade respectively, except for the highest grade (only greater than) and the lowest grade (only less than) */
+/*For Step 2, I will need a function that returns the corresponding letter grade, so it will need to check if the variable is both greater and less than the lower or higher grade respectively, except for the highest grade (only = 100) and the lowest grade (only less than) */
 
+if variable === 100, then return A++
+if variable < 100 but >= 90, then return A
+if variable < 90 but >= 80, then return B
+if variable < 80 but >= 70, then return C
+if variable < 70 but >= 60, then return D
+if variable < 60, then return F
+
+function getGrade(score) {
+
+}
